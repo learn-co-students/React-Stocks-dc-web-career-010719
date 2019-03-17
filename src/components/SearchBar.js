65cdaaebfb876386bd7e732226bdb6d4e91c854b
Anthony Gregg <agregg22@gmail.com>
2019-03-17 14:50:37 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,42 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = props => {
   return (
     <div>
-
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input
+          type="radio"
+          value="Alphabetically"
+          name="sort"
+          checked={null}
+          onChange={e => props.handleSort(e)}
+        />
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input
+          type="radio"
+          value="Price"
+          name="sort"
+          checked={null}
+          onChange={e => props.handleSort(e)}
+        />
         Price
       </label>
-      <br/>
+      <br />
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={e => props.handleFilter(e.target.value)}>
+          <option value="All">Please Select a Filter</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
         </select>
       </label>
-
-
     </div>
   );
-}
-
+};
 
 export default SearchBar;
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import Stock from '../components/Stock'
+import Stock from '../components/Stock';
 
 class StockContainer extends Component {
-
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.props.allStocks.map(stock => (
+          <Stock
+            key={`stock-${stock.id}`}
+            handleClick={this.props.handleClick}
+            stock={stock}
+          />
+        ))}
       </div>
     );
   }
-
 }
 
 export default StockContainer;
