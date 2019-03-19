8461cf619390b0de368df768cbf2774cb509814d
Heloise Taillet <heloise96@hotmail.com>
2019-03-19 19:40:44 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,37 @@
 import React from 'react';
 
-const SearchBar = () => {
+class SearchBar extends React.Component {
+  state = {alphabetChecked: false, priceChecked:false}
+
+  alphabetChange = () => {
+    this.props.sortAlphabetically(!this.state.alphabetChecked)
+    this.setState({alphabetChecked: !this.state.alphabetChecked, priceChecked:false})
+  }
+
+  priceChange = () => {
+    this.props.sortByPrice(!this.state.alphabetChecked)
+    this.setState({priceChecked: !this.state.priceChecked,alphabetChecked: false})
+  }
+
+
+  render() {
   return (
     <div>
-
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" name="sort" checked={this.state.alphabetChecked} onChange={this.alphabetChange}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" name="sort" checked={this.state.priceChecked} onChange={this.priceChange}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={this.props.filter}>
+        <option value="No Filter">None</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
@@ -27,6 +41,7 @@ const SearchBar = () => {
 
     </div>
   );
+}
 }
 
 
